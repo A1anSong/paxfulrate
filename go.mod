92b67adaf641e18c9acd10176dd5fdf56f8c0cec
A1an <387805107@qq.com>
2019-08-19 03:29:29 +0800

    add features
    use gin web framework to provide RESTFul service

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/A1anSong/paxfulrate
 go 1.12
 
 require (
+	github.com/gin-gonic/gin v1.4.0 // indirect
 	github.com/lib/pq v1.2.0 // indirect
 	github.com/tidwall/gjson v1.3.2 // indirect
 )
--- main.go
+++ main.go
@@ -6,8 +6,10 @@ import (
 	"database/sql"
 	"encoding/hex"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/tidwall/gjson"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
 	"strings"
@@ -128,9 +130,7 @@ func getGiftCardUSD() float64 {
 
 func calculatePaxfulrate() {
 	db, err := sql.Open("postgres", "host=localhost port=5432 user=postgres password=1314 dbname=paxfulrate sslmode=disable")
-	if err != nil {
-		panic(err)
-	}
+	checkErr(err)
 	defer db.Close()
 
 	var rateMinutes []float64
@@ -150,13 +150,9 @@ func calculatePaxfulrate() {
 						rateDay = sum / float64(len(rateHours))
 					}
 					stmt, err := db.Prepare(`insert into "rateDay"("rateDay", rate) values ($1, $2)`)
-					if err != nil {
-						panic(err)
-					}
+					checkErr(err)
 					_, err = stmt.Exec(now.Format("2006-01-02"), fmt.Sprintf("%.2f", rateDay))
-					if err != nil {
-						panic(err)
-					}
+					checkErr(err)
 
 					rateHours = rateHours[0:0]
 				}
@@ -169,13 +165,9 @@ func calculatePaxfulrate() {
 					rateHour = sum / float64(len(rateMinutes))
 				}
 				stmt, err := db.Prepare(`insert into "rateHour"("rateHour", rate) values ($1, $2)`)
-				if err != nil {
-					panic(err)
-				}
+				checkErr(err)
 				_, err = stmt.Exec(now.Format("2006-01-02 15:00:00"), fmt.Sprintf("%.2f", rateHour))
-				if err != nil {
-					panic(err)
-				}
+				checkErr(err)
 
 				if rateHour != 0 {
 					rateHours = append(rateHours, rateHour)
@@ -190,13 +182,9 @@ func calculatePaxfulrate() {
 				rate = bitcoinPrice / giftCardPrice
 			}
 			stmt, err := db.Prepare(`insert into "rateMinute"("rateMinute", rate, "giftcardPrice", "bitcoinPrice") values ($1, $2, $3, $4)`)
-			if err != nil {
-				panic(err)
-			}
+			checkErr(err)
 			_, err = stmt.Exec(now.Format("2006-01-02 15:04:00"), fmt.Sprintf("%.2f", rate), fmt.Sprintf("%.2f", giftCardPrice), fmt.Sprintf("%.2f", bitcoinPrice))
-			if err != nil {
-				panic(err)
-			}
+			checkErr(err)
 
 			if rate != 0 {
 				rateMinutes = append(rateMinutes, rate)
@@ -206,10 +194,116 @@ func calculatePaxfulrate() {
 	}
 }
 
+type RateMinute struct {
+	RateMinute string  `json:"rateMinute"`
+	Rate       float64 `json:"rate"`
+}
+
+type RateHour struct {
+	RateHour string  `json:"rateHour"`
+	Rate     float64 `json:"rate"`
+}
+
+type RateDay struct {
+	RateDay string  `json:"rateDay"`
+	Rate    float64 `json:"rate"`
+}
+
+func rateMinutes(c *gin.Context) {
+	db, err := sql.Open("postgres", "host=localhost port=5432 user=postgres password=1314 dbname=paxfulrate sslmode=disable")
+	checkErr(err)
+	defer db.Close()
+
+	rows, err := db.Query(`select * from (select "rateMinute", rate from "rateMinute" order by "rateMinute" desc limit 1440) as result order by "rateMinute"`)
+	checkErr(err)
+	defer rows.Close()
+
+	rateMinutes := make([]RateMinute, 0)
+	for rows.Next() {
+		var rateMinute RateMinute
+		var timeStr string
+		rows.Scan(&timeStr, &rateMinute.Rate)
+		tt, _ := time.Parse("2006-01-02T15:04:05Z", timeStr)
+		rateMinute.RateMinute = tt.Format("2006/01/02 15:04")
+		rateMinutes = append(rateMinutes, rateMinute)
+	}
+	if err = rows.Err(); err != nil {
+		log.Fatalln(err)
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"rateminutes": rateMinutes,
+	})
+}
+
+func rateHours(c *gin.Context) {
+	db, err := sql.Open("postgres", "host=localhost port=5432 user=postgres password=1314 dbname=paxfulrate sslmode=disable")
+	checkErr(err)
+	defer db.Close()
+
+	rows, err := db.Query(`select * from (select * from "rateHour" order by "rateHour" desc limit 1440) as result order by "rateHour"`)
+	checkErr(err)
+	defer rows.Close()
+
+	rateHours := make([]RateHour, 0)
+	for rows.Next() {
+		var rateHour RateHour
+		var timeStr string
+		rows.Scan(&timeStr, &rateHour.Rate)
+		tt, _ := time.Parse("2006-01-02T15:04:05Z", timeStr)
+		rateHour.RateHour = tt.Format("2006/01/02 15:04")
+		rateHours = append(rateHours, rateHour)
+	}
+	if err = rows.Err(); err != nil {
+		log.Fatalln(err)
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"ratehours": rateHours,
+	})
+}
+
+func rateDays(c *gin.Context) {
+	db, err := sql.Open("postgres", "host=localhost port=5432 user=postgres password=1314 dbname=paxfulrate sslmode=disable")
+	checkErr(err)
+	defer db.Close()
+
+	rows, err := db.Query(`select * from (select * from "rateDay" order by "rateDay" desc limit 180) as result order by "rateDay"`)
+	checkErr(err)
+	defer rows.Close()
+
+	rateDays := make([]RateDay, 0)
+	for rows.Next() {
+		var rateDay RateDay
+		var timeStr string
+		rows.Scan(&timeStr, &rateDay.Rate)
+		tt, _ := time.Parse("2006-01-02T15:04:05Z", timeStr)
+		rateDay.RateDay = tt.Format("2006/01/02")
+		rateDays = append(rateDays, rateDay)
+	}
+	if err = rows.Err(); err != nil {
+		log.Fatalln(err)
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"ratedays": rateDays,
+	})
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
 	go calculatePaxfulrate()
 
-	for {
-		time.Sleep(time.Hour * 1)
-	}
+	router := gin.Default()
+
+	router.GET("/rateminutes", rateMinutes)
+	router.GET("/ratehours", rateHours)
+	router.GET("/ratedays", rateDays)
+
+	router.Run(":8000")
 }
